8ee25d518ef655bf2a66ecb25901f53c03108f9e
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-08-09 13:21:01 +0300

    Add useState to AddUser.js

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import styles from './AddUser.module.css';
 import Button from '../UI/Button';
 
 const AddUser = (props) => {
+    const [enteredValue, setEnteredValue] = useState('');
+    const [isValid, setIsValid] = useState(true);
+
+    const goalInputChangeHandler = event => {
+        if (event.target.value.trim().length > 0) {
+          setIsValid(true);
+        }
+        setEnteredValue(event.target.value);
+      };
 
     const addUserHandler = (event) => {
-        event.preventDefault(); 
+        event.preventDefault();
+        if (enteredValue.trim().length === 0) {
+            setIsValid(false);
+            return;
+          }
     };
 
     return(
         <Card className={styles.input}>
             <form onSubmit={addUserHandler}>
                 <label htmlFor='username'>Username</label>
-                <input id='username' type='text'/>
+                <input 
+                    id='username' 
+                    type='text'
+                    onChange={goalInputChangeHandler}/>
                 <label htmlFor='userage'>Age (Years)</label>
                 <input id='age' type='number'/>
                 <Button type='age'>Add User</Button>
