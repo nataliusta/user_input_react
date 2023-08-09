be46ab54256110b0f0715127f71cbb0ac98780e9
Nataligit config --global user.email natalja_daad@tut.by <natalja_daad@tut.by>
2023-08-09 16:58:40 +0300

    Update AddUser.js

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import Card from '../UI/Card';
 import styles from './AddUser.module.css';
 import Button from '../UI/Button';
+import ErrorModal from '../UI/ErrorModal';
 
 const AddUser = (props) => {
     const [enteredValue, setEnteredValue] = useState('');
@@ -30,23 +31,26 @@ const AddUser = (props) => {
     };
 
     return(
-        <Card className={styles.input}>
-            <form onSubmit={addUserHandler}>
-                <label htmlFor='username'>Username</label>
-                <input 
-                    id='username' 
-                    type='text'
-                    value={enteredValue} // ?
-                    onChange={usernameChangeHandler}/>
-                <label htmlFor='userage'>Age (Years)</label>
-                <input 
-                    id='age' 
-                    type='number'
-                    value={enteredAge}
-                    onChange={addAgeHandler}/>
-                <Button type='age'>Add User</Button>
-            </form>
-        </Card>
+        <div>
+            <ErrorModal title='An error occured!' message='Something went wrong!' />
+            <Card className={styles.input}>
+                <form onSubmit={addUserHandler}>
+                    <label htmlFor='username'>Username</label>
+                    <input 
+                        id='username' 
+                        type='text'
+                        value={enteredValue} // ?
+                        onChange={usernameChangeHandler}/>
+                    <label htmlFor='userage'>Age (Years)</label>
+                    <input 
+                        id='age' 
+                        type='number'
+                        value={enteredAge}
+                        onChange={addAgeHandler}/>
+                    <Button type='age'>Add User</Button>
+                </form>
+            </Card>
+        </div>
     );
 };
 
